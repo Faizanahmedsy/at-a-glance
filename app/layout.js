807130c45177bf5e65f1af7411eae5d743a7a2ba
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-06 10:00:32 +0530

    Add NextUI components and implement a modal

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { cn } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className, "dark")}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,13 +1,85 @@
+"use client";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 import Image from "next/image";
 
 export default function Home() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <main className="min-h-screen grid grid-cols-3">
-      <div className="border-red-300 box-border border p-14">
-        <h2 className="text-3xl text-center font-bold">Faizans Todos</h2>
+      <div className="border-red-300 box-border  p-14">
+        <div className="flex text-center font-bold gap-4 justify-center">
+          <Avatar
+            isBordered
+            radius="lg"
+            src="https://i.pravatar.cc/150?u=a04258114e29026302d"
+          />
+          <h2 className="text-3xl">Faizans Todos</h2>
+        </div>
+
+        <Divider className="my-4 w-full bg-cyan-200" />
+
+        <Button
+          color="primary"
+          fullWidth
+          onPress={onOpen}
+          endContent={
+            <img src="https://api.iconify.design/lets-icons/add-duotone.svg?width=25&height=25" />
+          }
+        >
+          Add a task
+        </Button>
       </div>
       {/* <div>Faizan</div>
       <div>Faizan</div> */}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat
+                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
+                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
+                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
+                  eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </main>
   );
 }
